6e59973ba0e0f38c4a98f410312508068223d13c
Shun Nikiema <shun.nikiema@deeptechnologies.com>
2023-11-14 16:00:51 +0000

    15:59 14/11/2023

--- vipass/settings.py
+++ vipass/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'rest_framework',
-    'rest_framework_simplejwt.token_blacklist',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
