18b4db0949c7338386a1a788e4073ac969c9650b
Shun Nikiema <shun.nikiema@deeptechnologies.com>
2023-11-21 16:18:53 +0000

    16:18 21/11/2023

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 # Exemple pour urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AvailabilityViewSet, ClientViewSet, ServiceViewSet, IssueViewSet, SubscriptionViewSet, TicketViewSet, LogoutView, RegisterView, VerifyCodeView, ResendCodeView, SendEmailView, CustomTokenObtainPairView
+from .views import AvailabilityViewSet, ClientViewSet, ServiceViewSet, IssueViewSet, SubscriptionViewSet, TicketViewSet, \
+    LogoutView, RegisterView, VerifyCodeView, ResendCodeView, SendEmailView, CustomAuthToken
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
@@ -17,7 +18,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('verify-code/', VerifyCodeView.as_view(), name='verify-code'),
     path('resend-code/', ResendCodeView.as_view(), name='resend-code'),
-    path('login/', CustomTokenObtainPairView.as_view(), name='login'),
+    path('login/', CustomAuthToken.as_view(), name='login'),
     path('token-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('send-email/', SendEmailView.as_view(), name='send-email'),
--- vipass/settings.py
+++ vipass/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'rest_framework',
-    'rest_framework_simplejwt',
+    'rest_framework.authtoken',
     # 'rest_framework_simplejwt.authentication.JWTAuthentication',
 ]
 
